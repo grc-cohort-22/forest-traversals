88b725a0e0bcf818c7444317462e7c2878abc025
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-03-12 04:27:30 -0700

    fourth problem

--- src/TreeProblems.java
+++ src/TreeProblems.java
@@ -116,7 +116,14 @@ public class TreeProblems {
    Hint: There's a simple way to do this!
   */
   public static int sumTree(Map<Integer, List<Integer>> tree) {
-    return -1;
+    if(tree == null) return 0;
+
+    int sum = 0;
+
+    for(Integer node : tree.keySet()) {
+      sum += node;
+    }
+    return sum;
   }
 
   /*
