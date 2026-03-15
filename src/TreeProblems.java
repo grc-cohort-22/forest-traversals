36717470915ca3f0f7a89fa35ed3a9e71f7d8b3d
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-03-14 18:56:52 -0700

    done with findRoot, workin on maxDepth

--- src/TreeProblems.java
+++ src/TreeProblems.java
@@ -1,5 +1,7 @@
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class TreeProblems {
 
@@ -143,9 +145,22 @@ public class TreeProblems {
    Hint: No recursion needed! Think about how you would do this by hand.
   */
   public static <T> T findRoot(Map<T, List<T>> tree) {
-    for ()
+    Set<T> children = new HashSet<>();
+
+    for (List<T> list : tree.values()) {
+        for (T child : list) {
+            children.add(child);
+        }
+    }
+
+    for (T node : tree.keySet()) {
+        if (!children.contains(node)) {
+            return node;
+        }
+    }
+
     return null;
-  }
+}
 
   /*
    maxDepth (Node Version)
